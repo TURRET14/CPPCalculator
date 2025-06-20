dcb2a40933f9eb3a107ad4f75c249b1505af726b
Admin <Admin@SEMYONDESKTOP>
2025-06-20 15:39:05 +0300

    Added Exception Handling In Main Function

--- Source.cpp
+++ Source.cpp
@@ -4,8 +4,13 @@
 int main() {
     std::string Expression;
     while (true) {
-        std::cout << "Input Expression: ";
-        std::getline(std::cin, Expression);
-        std::cout << "Result: " << std::fixed << ExpressionCalculator::CalculateExpressionString(Expression) << std::endl;
+        try {
+            std::cout << "Input Expression: ";
+            std::getline(std::cin, Expression);
+            std::cout << "Result: " << std::fixed << ExpressionCalculator::CalculateExpressionString(Expression) << std::endl;
+        }
+        catch (std::exception Exception) {
+			std::cout << "Error: " << std::string(Exception.what()) << std::endl;
+        }
     }
 }
